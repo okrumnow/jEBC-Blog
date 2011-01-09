54f73b421f9b0dc75961a02978eb0a7ac90b3577
Olaf Krumnow <okrumnow@gmx.de>
2011-01-09 12:53:22 +0100

    refact

--- src/de/jebc/adressbook/activities/baumladen/BaumErstellen.java
+++ src/de/jebc/adressbook/activities/baumladen/BaumErstellen.java
@@ -2,7 +2,6 @@ package de.jebc.adressbook.activities.baumladen;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
@@ -14,25 +13,45 @@ import de.jebc.adressbook.domain.Name;
 
 public class BaumErstellen extends Process<List<Name>, TreeModel> {
 
+    private HashMap<String, DefaultMutableTreeNode> kategorien;
+    private DefaultMutableTreeNode root;
+
     @Override
     protected void process(List<Name> message) {
-        Map<String, DefaultMutableTreeNode> kategorien = new HashMap<String, DefaultMutableTreeNode>();
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode();
-        TreeModel result = new DefaultTreeModel(root);
-        for (Name entry : message) {
-            DefaultMutableTreeNode kategorieNode;
-            String kategorie = entry.getKategorie();
-            if (kategorien.containsKey(kategorie)) {
-                kategorieNode = kategorien.get(kategorie);
-            } else {
-                kategorieNode = new DefaultMutableTreeNode(kategorie);
-                kategorien.put(kategorie, kategorieNode);
-                root.add(kategorieNode);
-            }
-            MutableTreeNode nameNode = new DefaultMutableTreeNode(entry);
-            kategorieNode.add(nameNode);
-        }
+        TreeModel result = createTreeModel(message);
         Result().send(result);
+    }
+
+    private TreeModel createTreeModel(List<Name> message) {
+        TreeModel result = prepareEmptyTreeModel();
+        for (Name entry : message) {
+            addNameToTreeModel(entry);
+        }
+        return result;
+    }
+
+    private TreeModel prepareEmptyTreeModel() {
+        kategorien = new HashMap<String, DefaultMutableTreeNode>();
+        root = new DefaultMutableTreeNode();
+        return new DefaultTreeModel(root);
+    }
+
+    private void addNameToTreeModel(Name entry) {
+        DefaultMutableTreeNode kategorieNode = getOrCreateCategoryNode(entry.getKategorie());
+        MutableTreeNode nameNode = new DefaultMutableTreeNode(entry);
+        kategorieNode.add(nameNode);
+    }
+
+    private DefaultMutableTreeNode getOrCreateCategoryNode(String kategorie) {
+        DefaultMutableTreeNode kategorieNode;
+        if (kategorien.containsKey(kategorie)) {
+            kategorieNode = kategorien.get(kategorie);
+        } else {
+            kategorieNode = new DefaultMutableTreeNode(kategorie);
+            kategorien.put(kategorie, kategorieNode);
+            root.add(kategorieNode);
+        }
+        return kategorieNode;
     }
 
 }
