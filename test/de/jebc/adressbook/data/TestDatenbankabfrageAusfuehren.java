c7ce205b5fea8e38122abd32c38356d8b39dc448
Olaf Krumnow <okrumnow@gmx.de>
2011-01-09 11:17:30 +0100

    refact: using base class for another test

--- test/de/jebc/adressbook/data/TestDatenbankabfrageAusfuehren.java
+++ test/de/jebc/adressbook/data/TestDatenbankabfrageAusfuehren.java
@@ -9,14 +9,11 @@ import java.sql.Statement;
 
 import org.junit.Test;
 
-import de.jebc.InPin;
-import de.jebc.OutPin;
+import de.jebc.adressbook.activities.StoringPinsTestclass;
 import de.jebc.adressbook.domain.Abfrage;
 
-public class TestDatenbankabfrageAusfuehren {
-
-    protected ResultSet result;
-    protected Exception exception;
+public class TestDatenbankabfrageAusfuehren extends
+        StoringPinsTestclass<ResultSet> {
 
     @Test
     public void testAusfuehren() throws Exception {
@@ -25,21 +22,22 @@ public class TestDatenbankabfrageAusfuehren {
 
         DatenbankabfrageAusfuehren sut = new DatenbankabfrageAusfuehren(conn);
         storeResultPin(sut.Result());
-        
+
         sut.Start().receive(query);
 
         assertNotNull(result);
         assertTrue(result.next());
     }
-    
+
     @Test
-    public void testFehlerhafterBefehlSendetExceptionAnExceptionPin() throws Exception {
+    public void testFehlerhafterBefehlSendetExceptionAnExceptionPin()
+            throws Exception {
         Connection conn = setupDatabase();
         Abfrage query = new Abfrage("SELECT * FROM Unbekannt");
 
         DatenbankabfrageAusfuehren sut = new DatenbankabfrageAusfuehren(conn);
         storeExceptionPin(sut.Exception());
-        
+
         sut.Start().receive(query);
 
         assertNotNull(exception);
@@ -53,26 +51,6 @@ public class TestDatenbankabfrageAusfuehren {
         stmt.executeUpdate("CREATE TABLE Adressen (ID INTEGER, Name TEXT, Vorname TEXT, Anschrift TEXT, Telefon TEXT, Kategorie TEXT);");
         stmt.executeUpdate("INSERT INTO Adressen VALUES (1, 'Name', 'Vorname', 'Anschrift', 'Telefon', 'Privat')");
         return conn;
-    }
-
-    private void storeExceptionPin(OutPin<Exception> pin) {
-        pin.wire(new InPin<Exception>() {
-    
-            @Override
-            public void receive(Exception message) {
-                exception = message;
-            }
-        });
-    }
-
-    private void storeResultPin(OutPin<ResultSet> pin) {
-        pin.wire(new InPin<ResultSet>() {
-
-            @Override
-            public void receive(ResultSet message) {
-                result = message;
-            }
-        });
     }
 
 }
