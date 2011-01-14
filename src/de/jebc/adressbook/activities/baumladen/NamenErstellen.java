3bade51705ba5330688ad1183e2f89143234a8cd
Olaf Krumnow <okrumnow@gmx.de>
2011-01-14 16:50:21 +0100

    refact: extract methods

--- src/de/jebc/adressbook/activities/baumladen/NamenErstellen.java
+++ src/de/jebc/adressbook/activities/baumladen/NamenErstellen.java
@@ -13,19 +13,29 @@ public class NamenErstellen extends ProcessWithException<ResultSet, List<Name>> 
 
     @Override
     protected void process(ResultSet message) {
-        List<Name> result = new ArrayList<Name>();
         try {
-            while (message.next()) {
-                Name name = new Name(new Schluessel(message.getInt("ID")),
-                        String.format("%1$s, %2$s", message.getString("Name"),
-                                message.getString("Vorname")),
-                        message.getString("Kategorie"));
-                result.add(name);
-            }
+            List<Name> result = createNames(message);
             Result().send(result);
         } catch (SQLException e) {
             Exception().send(e);
         }
+    }
+
+    private List<Name> createNames(ResultSet message) throws SQLException {
+        List<Name> result = new ArrayList<Name>();
+        while (message.next()) {
+            Name name = createName(message);
+            result.add(name);
+        }
+        return result;
+    }
+
+    private Name createName(ResultSet message) throws SQLException {
+        Name name = new Name(new Schluessel(message.getInt("ID")),
+                String.format("%1$s, %2$s", message.getString("Name"),
+                        message.getString("Vorname")),
+                message.getString("Kategorie"));
+        return name;
     }
 
 }
